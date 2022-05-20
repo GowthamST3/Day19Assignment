1da301618b7d36094cf42917dc89e7f603a34f99
ST <gowtham.st@dxc.com>
2022-05-20 22:42:51 +0530

    Added - Code to validate given Mobile Number is valid or not

---
+++ MobileNoValidationUc4.java
@@ -0,0 +1,81 @@
+package com.BridgeLabz_Day19Assignment;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MobileNoValidationUc4 {
+	private static void validateUserFirstName() {
+
+	String firstNamePattern = "[A-Z]{1}[a-z]{2,}";
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("Enter First Name:");
+	String firstName = scanner.next();
+
+	Pattern pattern = Pattern.compile(firstNamePattern);
+	Matcher matcher = pattern.matcher(firstName);
+	boolean result = matcher.matches();
+
+	if(result == true)
+		validateUserLastName();
+	else
+		System.out.println("First Name is Invalid");
+}
+
+private static void validateUserLastName() {
+
+	String lastNamePattern = "[A-Z]{1}[a-z]{2,}";
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("Enter Last Name:");
+	String lastName = scanner.next();
+
+	Pattern pattern1 = Pattern.compile(lastNamePattern);
+	Matcher matcher1 = pattern1.matcher(lastName);
+	boolean result1 = matcher1.matches();
+
+	if(result1 == true)
+		validateUserEmail();
+	else
+		System.out.println("Last Name is invalid");
+}
+
+private static void validateUserEmail() {
+
+	String emailPattern = "[A-Za-z0-9.+-]+@[a-z0-9]+[.][a-z]";
+	System.out.println("Enter your Email:");
+	Scanner scanner = new Scanner(System.in);
+	String email = scanner.next();
+
+	Pattern pattern2 = Pattern.compile(emailPattern);
+	Matcher matcher2 = pattern2.matcher(email);
+	boolean result2 = matcher2.matches();
+
+	if(result2 == true)
+		validatreUserMobileNumber();
+	else
+		System.out.println("Email is invalid");
+}
+
+private static void validatreUserMobileNumber() {
+	
+	String mobileNoPattern = "(91-)?[6-9][0-9]{9}";
+	System.out.println("Enter your Mobile Number:");
+	Scanner scanner = new Scanner(System.in);
+	String mobileNumber = scanner.next();
+
+	Pattern pattern3 = Pattern.compile(mobileNoPattern);
+	Matcher matcher3 = pattern3.matcher(mobileNumber);
+	boolean result3 = matcher3.matches();
+
+	if(result3 == true)
+		System.out.println("Mobile Number is Valid");
+	else
+		System.out.println("Mobile Number is invalid");
+}
+
+public static void main(String[] args) {
+	System.out.println("Welcome to User Registration program");
+	validateUserFirstName();
+}
+
+}
